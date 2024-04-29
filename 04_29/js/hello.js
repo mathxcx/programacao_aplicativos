ef45d3b306149cf360ce057a8fa02ddbfefee91f
Matheus Sarti Brunelli <matheus_sbrunelli@hotmail.com>
2024-04-29 17:21:12 -0300

    Trabalhando com JavaScript

---
+++ 04_29/js/hello.js
@@ -0,0 +1,33 @@
+alert("Segundou")
+
+// Meu primeiro comentário
+
+/* Meu segundo comentário 
+Tudo certo por aqui!
+*/
+
+//Criando uma variável
+let texto = "Aula JavaScript"
+console.log(texto)
+let decimal = 20.8
+console.log(decimal)
+let inteiro = 20
+console.log(inteiro)
+let booleano = true
+console.log(booleano)
+
+//Para criar uma variável em JS não é necessário definir um tipo
+//podendo fazer a definição direta e atribuição em seguida
+
+
+document.write(texto) //Escrevendo dentro da página html
+
+let num1 = "10"
+let num2 = 20
+let soma = num1 + num2 //Concatenando string "10" + 20 resultando 1020
+console.log(soma)
+
+let dia = "Segunda"
+let saudacao = "Boa Tarde"
+console.log(dia+saudacao)
+
